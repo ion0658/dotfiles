ffc0700c656c050b26e47e3d0fc20f9e3a1f5558
ion <y0a6s5u8soy@gmail.com>
2024-07-19 20:37:46 +0900

    Update keymaps.lua
    buffer delete keymap is changed to function

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -9,6 +9,45 @@ map("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
 map("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
 map("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
 
+local function delete_current_bufer()
+    local buf = vim.api.nvim_get_current_buf() or 0
+    if vim.bo.modified then
+        local choice = vim.fn.confirm(("Save changes to %q?"):format(vim.fn.bufname()), "&Yes\n&No\n&Cancel")
+        if choice == 0 or choice == 3 then -- 0 for <Esc>/<C-c> and 3 for Cancel
+            return
+        end
+        if choice == 1 then -- Yes
+            vim.cmd.write()
+        end
+    end
+
+    for _, win in ipairs(vim.fn.win_findbuf(buf)) do
+        vim.api.nvim_win_call(win, function()
+            if not vim.api.nvim_win_is_valid(win) or vim.api.nvim_win_get_buf(win) ~= buf then
+                return
+            end
+            -- Try using alternate buffer
+            local alt = vim.fn.bufnr("#")
+            if alt ~= buf and vim.fn.buflisted(alt) == 1 then
+                vim.api.nvim_win_set_buf(win, alt)
+                return
+            end
+
+            -- Try using previous buffer
+            local has_previous = pcall(vim.cmd, "bprevious")
+            if has_previous and buf ~= vim.api.nvim_win_get_buf(win) then
+                return
+            end
+
+            -- Create new listed buffer
+            local new_buf = vim.api.nvim_create_buf(true, false)
+            vim.api.nvim_win_set_buf(win, new_buf)
+        end)
+    end
+    if vim.api.nvim_buf_is_valid(buf) then
+        pcall(vim.cmd, "bdelete! " .. buf)
+    end
+end
 -- buffers
 map("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
 map("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
@@ -16,7 +55,7 @@ map("n", "[b", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
 map("n", "]b", "<cmd>bnext<cr>", { desc = "Next Buffer" })
 map("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
 map("n", "<leader>`", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
-map("n", "<leader>bd", "<cmd>bd<cr>", { desc = "Delete Buffer" })
+map("n", "<leader>bd", delete_current_bufer, { desc = "Delete Buffer" })
 map("n", "<leader>bD", "<cmd>:bd<cr>", { desc = "Delete Buffer and Window" })
 
 -- Clear search with <esc>
