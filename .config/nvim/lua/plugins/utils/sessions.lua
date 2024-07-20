bf07bd8033e5726b493956c461025377b3f61b8c
ion <y0a6s5u8soy@gmail.com>
2024-07-20 20:12:41 +0900

    Update: sessions.nvim config

--- .config/nvim/lua/plugins/utils/sessions.lua
+++ .config/nvim/lua/plugins/utils/sessions.lua
@@ -2,5 +2,17 @@ return {
     "natecraddock/sessions.nvim",
     events = { "WinEnter" },
     lazy = true,
+    opts = {
+        auto_session_suppress_dirs = { "~/workspace", "~/Downloads", "/", "~/AppData", "~/Documents" },
+        auto_session_enable_last_session = true,
+        auto_restore_enabled = true,
+        auto_save_enabled = true,
+        session_lens = {
+            load_on_setup = false,
+            theme_conf = { border = true },
+            previewer = false,
+            buftypes_to_ignore = {},
+        },
+    },
     config = true
 }
