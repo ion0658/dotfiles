b2fb3a22a623d501e7b9bfd992be701aad4df537
ion <y0a6s5u8soy@gmail.com>
2024-07-20 20:56:09 +0900

    fix autosession restore

---
+++ .config/nvim/lua/plugins/utils/auto_session.lua
@@ -0,0 +1,24 @@
+return {
+    {
+        'rmagatti/auto-session',
+        dependencies = {
+            'nvim-telescope/telescope.nvim', -- Only needed if you want to use sesssion lens
+        },
+        opts = {
+            auto_session_suppress_dirs = { "~/workspace", "~/Downloads", "/", "~/AppData", "~/Documents" },
+            auto_session_enable_last_session = false,
+            auto_restore_enabled = true,
+            auto_save_enabled = true,
+            auto_restore_lazy_delay_enabled = true,
+            session_lens = {
+                load_on_setup = false,
+                theme_conf = { border = true },
+                previewer = false,
+                buftypes_to_ignore = {},
+            },
+        },
+        config = function(_, opts)
+            require('auto-session').setup(opts)
+        end,
+    },
+}
