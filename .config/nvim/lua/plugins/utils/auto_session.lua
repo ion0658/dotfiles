061ff2c342bfdb0e3613fa8b1aea84883ecd0311
ion <y0a6s5u8soy@gmail.com>
2024-07-22 00:03:08 +0900

    Update: auto_session.lua
    Update the sessionoptions to include 'localoptions' to save the local options of the buffer.

--- .config/nvim/lua/plugins/utils/auto_session.lua
+++ .config/nvim/lua/plugins/utils/auto_session.lua
@@ -38,6 +38,7 @@ return {
             }
         },
         config = function(_, opts)
+            vim.o.sessionoptions = 'blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions'
             require('auto-session').setup(opts)
         end,
     },
