81bd8b82ae95de74105a1f8e118d53602018c050
oh Ion <y0a6s5u8soy@gmail.com>
2024-07-24 11:53:46 +0900

    wip

--- .config/nvim/lua/plugins/lsp/lsp_signature.lua
+++
@@ -1,8 +0,0 @@
-return {
-    {
-        "ray-x/lsp_signature.nvim",
-        event = "LspAttach",
-        opts = {},
-        config = function(_, opts) require 'lsp_signature'.setup(opts) end
-    }
-}
