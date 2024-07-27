c4dfe88bdf640ab563863a6bec326b924a5c7f85
ion <y0a6s5u8soy@gmail.com>
2024-07-28 00:43:55 +0900

    fix volar settings

--- .config/nvim/lua/plugins/lsp/mason.lua
+++ .config/nvim/lua/plugins/lsp/mason.lua
@@ -21,8 +21,34 @@ return {
             p.setup_handlers {
                 function(server_name)
                     local server_config = {}
+                    local tsdk = function()
+                        return vim.fn.getcwd() .. "/node_modules/typescript/lib"
+                    end
+
                     if server_name == "volar" then
-                        server_config.filetypes = { 'vue', 'typescript', 'javascript' }
+                        server_config = {
+                            filetypes = { "vue" },
+                            init_options = {
+                                vue = {
+                                    hybridMode = false
+                                },
+                            }
+                        }
+                    end
+                    if server_name == "tsserver" then
+                        local mason_packages = vim.fn.stdpath("data") .. "/mason/packages"
+                        local volar_path = mason_packages .. "/vue-language-server/node_modules/@vue/language-server"
+                        server_config = {
+                            init_options = {
+                                plugins = {
+                                    {
+                                        name = "@vue/typescript-plugin",
+                                        location = volar_path,
+                                        languages = { "vue" },
+                                    },
+                                },
+                            },
+                        }
                     end
                     require("lspconfig")[server_name].setup(server_config)
                 end
