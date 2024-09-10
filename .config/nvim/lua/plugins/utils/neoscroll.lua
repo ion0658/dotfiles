ac8c631c3f005710f359d51aa8cd2551aef102ae
unknown <y0a6s5u8soy@gmail.com>
2024-09-10 10:09:06 +0900

    wip

--- .config/nvim/lua/plugins/utils/neoscroll.lua
+++ .config/nvim/lua/plugins/utils/neoscroll.lua
@@ -9,13 +9,5 @@ return {
                 "<C-u>", "<C-d>", "<C-b>", "<C-f>"
             }
         },
-        config = function()
-            local t = {}
-            t['<C-u>'] = { 'scroll', { '-vim.wo.scroll', 'true', '125' } }
-            t['<C-d>'] = { 'scroll', { 'vim.wo.scroll', 'true', '125' } }
-            t['<C-b>'] = { 'scroll', { '-vim.api.nvim_win_get_height(0)', 'true', '250' } }
-            t['<C-f>'] = { 'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '250' } }
-            require('neoscroll.config').set_mappings(t)
-        end
     },
 }
