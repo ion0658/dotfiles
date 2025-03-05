c6375ae8a0b3be08838602e32d17e2e12d14b7c5
ion oh <y0a6s5u8soy@gmail.com>
2025-03-05 18:09:27 +0900

    wip

--- .config/nvim/lua/plugins/lsp/nvim_lspconfig.lua
+++ .config/nvim/lua/plugins/lsp/nvim_lspconfig.lua
@@ -9,14 +9,10 @@ return {
         },
         opts = {
             servers = {
-                sourcekit = {
-                    capabilities = {
-                        workspace = {
-                            didChangeWatchedFiles = {
-                                dynamicRegistration = true,
-                            },
-                        },
-                    },
+                rust_analyzer = {
+                    cachePriming = {
+                        enable = false
+                    }
                 }
             }
         },
@@ -28,26 +24,4 @@ return {
             end
         end
     },
-    {
-        "ray-x/lsp_signature.nvim",
-        version = false,
-        lazy = true,
-        event = { "BufReadPre", "BufNewFile", "BufWritePre" },
-        opts = {},
-        config = function(_, opts)
-            require 'lsp_signature'.setup(opts)
-            vim.api.nvim_create_autocmd("LspAttach", {
-                callback = function(args)
-                    local bufnr = args.buf
-                    local client = vim.lsp.get_client_by_id(args.data.client_id)
-                    if vim.tbl_contains({ 'null-ls' }, client.name) then -- blacklist lsp
-                        return
-                    end
-                    require("lsp_signature").on_attach({
-                        -- ... setup options here ...
-                    }, bufnr)
-                end,
-            })
-        end
-    }
 }
