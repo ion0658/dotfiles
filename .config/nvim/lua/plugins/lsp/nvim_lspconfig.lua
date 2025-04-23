2f4627eb8bae0c387a85e47a527a903d82fee750
IonOH <y0a6s5u8soy@gmail.com>
2025-04-23 18:34:22 +0900

    wip

--- .config/nvim/lua/plugins/lsp/nvim_lspconfig.lua
+++ .config/nvim/lua/plugins/lsp/nvim_lspconfig.lua
@@ -24,7 +24,7 @@ return {
                     rust_analyzer = {
                         settings = {
                             cachePriming = {
-                                enable = false
+                                enable = true
                             }
                         },
                     },
