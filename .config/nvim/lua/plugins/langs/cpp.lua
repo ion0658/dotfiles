c60e3e2bc95ec3ebd59d4bf3aba28a5de184dc7a
IonOH <7760008+ion0658@users.noreply.github.com>
2025-11-01 12:32:53 +0900

    change cmake lsp to neocmake

--- .config/nvim/lua/plugins/langs/cpp.lua
+++ .config/nvim/lua/plugins/langs/cpp.lua
@@ -22,7 +22,7 @@ return {
         opts = {
             ensure_installed = {
                 "clang-format",
-                "cmake-language-server"
+                "neocmakelsp"
             }
         }
     },
