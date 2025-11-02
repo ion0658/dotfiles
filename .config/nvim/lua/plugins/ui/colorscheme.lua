b09e04b3f1d7aa69ae5cc09b0e5a903e94783fda
IonOH <7760008+ion0658@users.noreply.github.com>
2025-11-02 14:14:31 +0900

    update color scheme

--- .config/nvim/lua/plugins/ui/colorscheme.lua
+++ .config/nvim/lua/plugins/ui/colorscheme.lua
@@ -5,9 +5,9 @@ return {
         enabled = true,
         priority = 8000, -- Very high priority is required
         config = function()
-            vim.g.sonokai_enable_italic = true
-            vim.g.sonokai_style = "andromeda"
-            vim.g.sonokai_better_performance = true
+            vim.g.sonokai_enable_italic = 1
+            vim.g.sonokai_style = "atlantis"
+            vim.g.sonokai_better_performance = 1
             vim.g.sonokai_transparent_background = 2
             vim.cmd.colorscheme("sonokai")
         end
@@ -27,19 +27,5 @@ return {
             vim.cmd.colorscheme("tokyonight")
         end
     },
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        enabled = false,
-        lazy = false,
-        priority = 8000,
-        opts = {
-            flavour = "mocha",
-            transparent_background = true,
-        },
-        config = function(_, opts)
-            require("catppuccin").setup(opts)
-            vim.cmd.colorscheme("catppuccin")
-        end
-    }
+
 }
