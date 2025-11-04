576522fd030c644675349881f35e748f1cdb6fd5
IonOH <7760008+ion0658@users.noreply.github.com>
2025-11-04 22:49:57 +0900

    add vim-doge

---
+++ .config/nvim/lua/plugins/coding/doc_gen.lua
@@ -0,0 +1,28 @@
+return {
+    {
+        "kkoomen/vim-doge",
+        enabled = true,
+        version = "*",
+        lazy    = true,
+        event   = { 'BufReadPost', 'BufWritePost', 'BufNewFile' },
+        opts    = {
+            doge_doc_standard_cpp = 'doxygen_cpp_comment_slash',
+            doge_doc_standard_c = 'doxygen_cpp_comment_slash',
+        },
+        keys    = {
+            { "<leader>gd", "<cmd>DogeGenerate<cr>", desc = "Generate Documentation" },
+        },
+        init    = function()
+            vim.g.doge_mapping = 0
+            vim.g.doge_enable_mappings = 0
+            vim.g.doge_buffer_mappings = 0
+            vim.g.doge_mapping_comment_jump_forward = 0
+            vim.g.doge_mapping_comment_jump_backward = 0
+        end,
+        config  = function(_, opts)
+            vim.g.doge_doc_standard_cpp = opts.doge_doc_standard_cpp
+            vim.g.doge_doc_standard_c = opts.doge_doc_standard_c
+        end,
+    }
+}
+
