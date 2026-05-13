40f091849435bf2c18c0f7f318839bb968f4f950
Ion OH <y0a6s5u8soy@gmail.com>
2026-05-13 14:14:47 +0900

    change color scheme

--- .config/nvim/lua/plugins/ui/colorscheme.lua
+++ .config/nvim/lua/plugins/ui/colorscheme.lua
@@ -2,7 +2,7 @@ return {
     {
         "sainnhe/sonokai",
         version = "*",
-        enabled = true,
+        enabled = false,
         priority = 8000, -- Very high priority is required
         config = function()
             vim.g.sonokai_enable_italic = 1
@@ -27,5 +27,34 @@ return {
             vim.cmd.colorscheme("tokyonight")
         end
     },
-
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        version = "*",
+        enabled = false,
+        priority = 8000,
+        opts = {
+            flavour = "mocha",
+            transparent_background = true,
+        },
+        config = function(_, opts)
+            require("catppuccin").setup(opts)
+            vim.cmd.colorscheme("catppuccin")
+        end
+    },
+    {
+        "rebelot/kanagawa.nvim",
+        version = "*",
+        enabled = true,
+        priority = 8000,
+        opts = {
+            compile = true,   -- enable compiling the colorscheme
+            undercurl = true, -- enable undercurls
+            theme = "wave",
+        },
+        config = function(_, opts)
+            require("kanagawa").setup(opts)
+            vim.cmd.colorscheme("kanagawa")
+        end
+    }
 }
